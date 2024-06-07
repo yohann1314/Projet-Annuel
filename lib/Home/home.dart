a0e03e408a27c8f61b0b998ca62bf3870488968c
gedeon <gedeonmutikanga@gmail.com>
2024-06-07 12:23:44 +0200

    Affichage de laliste defilante des rectangles d'événements

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -104,6 +104,9 @@ class MainApp extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20), // Espace entre la liste d'icônes et le texte
+            Expanded(
+              child: EventListView(), // Ajout du bloc d'événements
+            ),
           ],
         ),
       ),
@@ -152,3 +155,139 @@ class IconWithText extends StatelessWidget {
     );
   }
 }
+
+class Event {
+  final String title;
+  final String description;
+  final String date;
+  final String location;
+  final String style;
+  final String imageUrl;
+
+  Event({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.location,
+    required this.style,
+    required this.imageUrl,
+  });
+}
+
+class EventListView extends StatelessWidget {
+  final List<Event> events = [
+    Event(
+      title: "Festival EVENTHUB",
+      description: "Description de l'événement",
+      date: "Jeudi 18 Avril 2024",
+      location: "10 Rue Réné Viviani 44000 Nantes",
+      style: "Populaire",
+      imageUrl: "OIP.jpg", // Insérez l'URL de votre image ici
+    ),
+    Event(
+      title: "Autre événement",
+      description: "Description de l'événement",
+      date: "Vendredi 19 Avril 2024",
+      location: "20 Rue de Strasbourg 44000 Nantes",
+      style: "Festif",
+      imageUrl: "https://th.bing.com/th/id/OIP.bOYyTEu7iYJZfczpU7_81wAAAA?rs=1&pid=ImgDetMain", // Insérez l'URL de votre image ici
+    ),
+    Event(
+      title: "Autre événement",
+      description: "Description de l'événement",
+      date: "Vendredi 19 Avril 2024",
+      location: "20 Rue de Strasbourg 44000 Nantes",
+      style: "Festif",
+      imageUrl: "https://th.bing.com/th/id/OIP.bOYyTEu7iYJZfczpU7_81wAAAA?rs=1&pid=ImgDetMain", // Insérez l'URL de votre image ici
+    ),
+    Event(
+      title: "Autre événement",
+      description: "Description de l'événement",
+      date: "Vendredi 19 Avril 2024",
+      location: "20 Rue de Strasbourg 44000 Nantes",
+      style: "Festif",
+      imageUrl: "https://th.bing.com/th/id/OIP.bOYyTEu7iYJZfczpU7_81wAAAA?rs=1&pid=ImgDetMain", // Insérez l'URL de votre image ici
+    ),
+    Event(
+      title: "Autre événement",
+      description: "Description de l'événement",
+      date: "Vendredi 19 Avril 2024",
+      location: "20 Rue de Strasbourg 44000 Nantes",
+      style: "Festif",
+      imageUrl: "https://th.bing.com/th/id/OIP.bOYyTEu7iYJZfczpU7_81wAAAA?rs=1&pid=ImgDetMain", // Insérez l'URL de votre image ici
+    ),
+    // Ajoutez autant d'événements que vous le souhaitez
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: events.length,
+      itemBuilder: (context, index) {
+        final event = events[index];
+        return Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Affichage de l'image de l'événement avec un BorderRadius
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.network(
+                  event.imageUrl,
+                  //width: double.infinity,
+                  height: 250, // Ajustez la hauteur de l'image selon vos besoins
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Affichage du style de l'événement
+              Text(
+                event.style,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+              // Affichage du titre de l'événement
+              Text(
+                event.title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4.0),
+              // Affichage de la date de l'événement
+              Text(
+                event.date,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                ),
+              ),
+              const SizedBox(height: 4.0),
+              // Affichage du lieu de l'événement
+              Text(
+                event.location,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                ),
+              ),
+              const SizedBox(height: 4.0),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+}
+
